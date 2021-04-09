fd57488e47e74aff4ed15cbb4050096f94850060
israel larrondo <arsistyle@gmail.com>
2021-04-09 14:35:48 -0400

    12042021

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const BeautifyHtmlWebpackPlugin = require('beautify-html-webpack-plugin');
 //   path: path.join(__dirname, '/.env'),
 // });
 
-const HOME_DATE = '06042021';
+const HOME_DATE = '12042021';
 
 const isProduction =
   process.argv[process.argv.indexOf('--mode') + 1] === 'production';
